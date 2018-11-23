981e6dacba44f309803a6bf5349ebe7510eff093
Chianti Scarlett <chianti.scarlett@gmail.com>
2018-11-23 17:54:32 +0900

    Basic Framework sketch

---
+++ command.py
@@ -0,0 +1,15 @@
+class Command:
+    def __init__(self):
+        pass
+
+
+class ls(Command):
+    pass
+
+
+class ll(Command):
+    pass
+
+
+class mkdir(Command):
+    pass
---
+++ core.py
@@ -0,0 +1,3 @@
+# Run mainloop
+def mainloop():
+    pass
