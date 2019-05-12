bfeeb26d55760654d2d8dd3ea7c0ea19ab76d7ed
Chianti Scarlett <chianti.scarlett@gmail.com>
2019-05-12 17:33:19 +0900

    basic structure design complete

---
+++ objectHandler.py
@@ -0,0 +1,35 @@
+##############################################################################
+# Filename: objectHandler.py
+# Description: handle Synapse object.
+##############################################################################
+
+
+class Tree:
+    def __init__(self, root_synapse):
+        """
+        Class object that fabricates data based on the .root_synapse file.
+        """
+        pass
+
+    def draw(self, depth, head):
+        """
+        Description:
+            Print out tree structure on console, similar to the `tree`
+            linux command.
+        Arguments:
+            depth: file depth
+            head: number of lines to print
+        """
+        pass
+
+    def get_tags(self):
+        """
+        Print all the tags.
+        """
+        pass
+
+    def search_by_tag(self, tag):
+        """
+        Search Synapse objects that have specific tag
+        """
+        pass
