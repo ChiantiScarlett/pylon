744670c164a197453214369ec80f3be83cff44c0
Chianti Scarlett <chianti.scarlett@gmail.com>
2019-05-12 17:04:43 +0900

    abstract structure design complete

---
+++ dropbox.py
@@ -0,0 +1,37 @@
+class Dropbox:
+    def __init__(self):
+        """
+        Create a dropbox object via Dropbox API, and get the file.
+        """
+        pass
+
+    def initialize(self):
+        """
+        If this is the first time, create a Synapse folder and
+        create necessary files.
+        """
+        pass
+
+    def connect(self):
+        """
+        Connect Dropbox object with keys from settings.json.
+        """
+        pass
+
+    def create(self, path):
+        """
+        Create a new synapse object.
+        """
+        pass
+
+    def upload(self, path):
+        """
+        Upload an object to the Dropbox.
+        """
+        pass
+
+    def update(self, path):
+        """
+        Update .root_synapse based on the file change.
+        """
+        pass
